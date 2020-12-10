3265524fbb7259c8779b39b94b52eeaa71e0da97
NikitaDe <69841196+NikitaDe@users.noreply.github.com>
2020-12-10 14:22:32 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,16 @@
+// <script type="text/javascript">
+// let html = document.getElementsByClassName('html_progress')[0].style.background;
+// let css = document.getElementsByClassName('css_progress')[0].style.background;
+// let js = document.getElementsByClassName('js_progress')[0].style.background;
+// let python = document.getElementsByClassName('python_progress')[0].style.background;
+// window.onload = function(){
+//   AutoProgress();
+// }
+//
+// function AutoProgress() {
+//     html = 'linear-gradient(to right, #41C0C0 70%,rgba(255, 0, 0, 0) 30%)';
+//     css = 'linear-gradient(to right, #41C0C0 60%,rgba(255, 0, 0, 0) 40%)';
+//     js ='linear-gradient(to right, #41C0C0 40%,rgba(255, 0, 0, 0) 40%)' ;
+//     python = 'linear-gradient(to right, #41C0C0 50%,rgba(255, 0, 0, 0) 50%)';
+//   }
+// </script>
